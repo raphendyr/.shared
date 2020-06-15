024b5e0d7febcc3b21e590af10c34d3fb0a3b952
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2020-06-15 13:41:15 +0300

    zsh: show stashes in git and limit quilt

--- zsh/plugins/my-git-info/my-git-info.zsh
+++ zsh/plugins/my-git-info/my-git-info.zsh
@@ -1,26 +1,42 @@
 # Setup vcs_info
 autoload -Uz vcs_info
 
+# style
 zstyle ':vcs_info:*' actionformats '%F{5}(%F{3}%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
-zstyle ':vcs_info:*' formats '%F{5}(%F{3}%s%F{5})%F{3}-%F{5}[%F{2}%b%F{6}%m%F{5}]%f '
+zstyle ':vcs_info:*' formats       '%F{5}(%F{3}%s%F{5})%F{3}-%F{5}[%F{2}%b%F{6}%m%F{9}%u%F{5}]%f '
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
 zstyle ':vcs_info:*' enable git
+
+# quilt
+zstyle ':vcs_info:*' quilt-standalone +vi-quilt-active
+zstyle ':vcs_info:*' use-quilt true
+
+function +vi-quilt-active() {
+	[[ $PWD == /usr/src(|/*) && -f .pc/.version ]] && return 0
+	return 1
+}
 
 # git: Show +N/-N when your local branch is ahead-of or behind remote HEAD.
 zstyle ':vcs_info:git*+set-message:*' hooks git-st
 function +vi-git-st() {
-	local ahead behind
+	local ahead behind stashes
 	local -a gitstatus
 
+	# ahead/behind
 	ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l)
 	(( $ahead )) && gitstatus+=( "+${ahead}" )
-
 	behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2>/dev/null | wc -l)
 	(( $behind )) && gitstatus+=( "-${behind}" )
-
 	hook_com[misc]+=${(j:/:)gitstatus}
+
+	# stashes
+	#stashes=$(git stash list 2>/dev/null | wc -l)
+	stashes="${hook_com[base]}/.git/logs/refs/stash"
+	stashes=$(test -e "$stashes" && wc -l < "$stashes")
+	(( $stashes )) && hook_com[unstaged]="@$stashes"
 }
 
+# set RPS1 to vcs_info (this should be the first plugin touching that)
 function +vi-precmd() {
 	vcs_info
 	RPS1="$vcs_info_msg_0_"

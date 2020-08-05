a3b22037c966d6b74019298092d9c269669a65ab
Jaakko Kantojärvi <jaakko@n-1.fi>
2020-08-05 19:17:15 +0300

    zsh: fix zsh for macos

--- zsh/plugins/fzf/fzf.zsh
+++ zsh/plugins/fzf/fzf.zsh
@@ -1,23 +1,32 @@
 for f in \
 	/usr/share/doc/fzf/examples/key-bindings.zsh \
 	/usr/share/doc/fzf/examples/completion.zsh \
+	"$HOME/.local/lib/Homebrew/opt/fzf/shell/key-bindings.zsh" \
+	"$HOME/.local/lib/Homebrew/opt/fzf/shell/completion.zsh" \
 ; do
 	[ -e "$f" ] && . "$f"
 done; unset f
 
-if which fdfind >/dev/null; then
+if which fdfind >/dev/null || which fd >/dev/null; then
 	# Use fd (https://github.com/sharkdp/fd) instead of the default find
 	# NOTE: fdfind is the binary in Debian
 	# thse are used for **<TAB>
+
+	if which fdfind >/dev/null; then
+		FZF_FD_COMMAND='fdfind'
+	else
+		FZF_FD_COMMAND='fd'
+	fi
+
 	_fzf_compgen_path() {
 		echo "$1"
-		command fdfind -c always -HL -E ".git" . "$1"
+		command "$FZF_FD_COMMAND" -c always -HL -E ".git" . "$1"
 	}
 	_fzf_compgen_dir() {
-		command fdfind -c always -HL -E ".git" -t d . "$1"
+		command "$FZF_FD_COMMAND" -c always -HL -E ".git" -t d . "$1"
 	}
 
-	FZF_DEFAULT_COMMAND='fdfind -c always'
+	FZF_DEFAULT_COMMAND="$FZF_FD_COMMAND -c always"
 	FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND -t f --"
 	FZF_CTRL_T_OPTS='-m'
 	FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND -H -E .git -E __pycache__ -E venv -t d --"

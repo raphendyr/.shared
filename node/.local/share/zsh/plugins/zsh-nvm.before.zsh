4248efc80251c8188d4e67097d0d524b1dcf0705
Jaakko Kantojärvi <jaakko.kantojarvi@reaktor.com>
2023-08-23 17:39:40 +0300

    node: show node version in zsh RPS

--- node/.local/share/zsh/plugins/zsh-nvm.before.zsh
+++ node/.local/share/zsh/plugins/zsh-nvm.before.zsh
@@ -2,3 +2,17 @@
 export NVM_DIR="$HOME/.local/share/nvm"
 NVM_LAZY_LOAD=true
 NVM_COMPLETION=true
+
+function +zsh-nvm-precmd() {
+	if [[ $NVM_BIN ]]; then
+		if [[ -z $_ZSH_NVM_RPS || $NVM_BIN != ${_ZSH_NVM_BIN:-} ]]; then
+			_ZSH_NVM_BIN=$NVM_BIN
+			_ZSH_NVM_RPS="%F{4}◢ Node $(node --version)%k%f"
+		fi
+		RPS1="$_ZSH_NVM_RPS $RPS1"
+	elif [[ $_ZSH_NVM_RPS ]]; then
+		unset _ZSH_NVM_RPS
+	fi
+}
+
+add-zsh-hook precmd +zsh-nvm-precmd

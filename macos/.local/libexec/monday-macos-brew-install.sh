afebe06ee756770b07fd41385ee9ac36c331ff3a
Jaakko Kantojärvi <jaakko.kantojarvi@reaktor.com>
2024-01-03 13:46:18 +0200

    macos: Update monday upgrade script

--- macos/.local/libexec/monday-macos-brew-install.sh
+++ macos/.local/libexec/monday-macos-brew-install.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
-set -eu
 
-export SUDO_ASKPASS=/usr/bin/false
-shared_dir=$(cd ~/.local/bin; f=$(readlink "setup-macos.sh"); cd "${f%/*}/../.."; echo "$PWD")
+# export SUDO_ASKPASS=/usr/bin/false
+shared_dir=$(
+	f=$(command -v setup-macos.sh) \
+	&& cd "${f%/*}" \
+	&& f=$(readlink "$f") \
+	&& cd "${f%/*}/../.." \
+	&& echo "$PWD" \
+	|| exit 1
+)
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-if brew update \
+brew_upgrade() {
+	brew update \
 	&& brew bundle --file "$shared_dir/Brewfile" install \
-	&& brew upgrade --greedy \
-; then
+	&& brew upgrade --greedy
+}
+
+mas_upgrade() {
+	if ! command -v mas > /dev/null; then
+		echo "No 'mas' found. Skipping App Store updates."
+		return
+	fi
+	if [ "$(mas outdated)" ]; then
+		mas upgrade
+	fi
+}
+
+set -x
+if brew_upgrade && mas_upgrade; then
 	echo "All done!"
 else
 	echo "Something failed! Please fix."
